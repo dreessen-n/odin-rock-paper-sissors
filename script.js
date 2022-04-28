ed68973bd32d3202247b880154021333bff2e653
dreessen-n <dreessen.edu@gmail.com>
2022-04-28 15:50:47 -0600

    First commit of rock, paper, sissors project

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // script.js for odin - rock paper sissors
+
+function computerPlay() {
+
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+
+  }
+ 
+function game() {
+
+}  
+
+
+// TODO: write computerPlay() to get computerSelection
+const computerSelection = computerPlay();
+
+// get player selection
+const playerSelection = alert()
+
+
+// TODO: write playRound() to play a round, return string with result
+
+// TODO: write game() to play 5 rounds of game and keep score
+
+
+  console.log(playRound(playerSelection, computerSelection));
