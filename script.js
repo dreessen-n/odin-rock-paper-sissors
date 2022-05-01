27bcc3373eabe3d14de1cc7d7b60dd63fde8c8f5
dreessen-n <dreessen.edu@gmail.com>
2022-05-01 13:26:40 -0600

    Write the computerPlay() and playerSelection

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // script.js for odin - rock paper sissors
 
-function computerPlay() {
+const choices = ['Rock', 'Paper', 'Sissors'];
 
+function computerPlay() {
+   const choice = choices[Math.floor(Math.random() * choices.length)];
+   return choice;
 }
 
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-
+     
   }
  
 function game() {
@@ -19,10 +21,10 @@ function game() {
 const computerSelection = computerPlay();
 
 // get player selection
-const playerSelection = alert()
-
+const playerSelection = prompt('Please choose: Rock, Paper or Sissors');
 
 // TODO: write playRound() to play a round, return string with result
+const result = playRound(playerSelection, computerSelection);
 
 // TODO: write game() to play 5 rounds of game and keep score
 
