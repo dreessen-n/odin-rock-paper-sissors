378ca71ae9197908766064f1ce984979dab3f972
dreessen-n <dreessen.edu@gmail.com>
2022-05-02 09:31:22 -0600

    Fix spaces to tabs. Make formatting consistant. Also change all the console.log() lines to returns

--- script.js
+++ script.js
@@ -3,36 +3,36 @@
 const choices = ['Rock', 'Paper', 'Sissors'];
 
 function computerPlay() {
-   const choice = choices[Math.floor(Math.random() * choices.length)];
-   const compChoice = choice.toLowerCase();
-   return compChoice;
+    const choice = choices[Math.floor(Math.random() * choices.length)];
+    const compChoice = choice.toLowerCase();
+    return compChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log('Tie! Play again');
+        return 'Tie! Play again';
     } else if (playerSelection == 'rock') {
         if (computerSelection == 'sissors') {
-           console.log("You Win! Rock beats Sissors") 
-        } else if (computerSelection == 'paper') {
-            console.log("You Lose! Paper beats Rock")
+            return "you win! rock beats sissors"; 
+        } else if (computerselection == 'paper') {
+            return "You Lose! Paper beats Rock";
         }
     } else if (playerSelection == 'paper') {
         if (computerSelection == 'rock') {
-            console.log("You Win! Paper beats Rock")
+            return "You Win! Paper beats Rock";
         } else if (computerSelection == 'sissors') {
-            console.log("You Lose! Sissors beats Paper")
+            return "You Lose! Sissors beats Paper";
         }
     } else if (playerSelection == 'sissors') {
         if (computerSelection == 'rock') {
-            console.log("You Lose! Rock beats Sissors")
+            return "You Lose! Rock beats Sissors";
         } else if (computerSelection == 'paper') {
-            console.log("You Win! Sissors beats Paper")
+            return "You Win! Sissors beats Paper";
         }
     }
     console.log(playerSelection, computerSelection);
-  }
- 
+}
+
 function game() {
 
 }  
@@ -43,11 +43,11 @@ const computerSelection = computerPlay();
 
 // get player selection
 const pS = prompt('Please enter either: Rock, Paper or Sissors');
-   playerSelection = pS.toLowerCase();
-   console.log(playerSelection); 
+    playerSelection = pS.toLowerCase();
 
 // write playRound() to play a round, return string with result
 const result = playRound(playerSelection, computerSelection);
+console.log(result);
 
 // TODO: write game() to play 5 rounds of game and keep score
 
